f42d7a9c29afb1fbc1f84e113cd4d3fcac817ef7
Nesa Mouzehkesh <nesa.mouzehkesh@foxtel.com.au>
2021-11-09 11:44:13 +1100

    WEB-1417, well it is now asking for customSyntax again dahhh

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const strictValueDefaults = ['inherit', 'initial', 'unset'];
 
 // See https://stylelint.io/user-guide/configure
 module.exports = {
+    "customSyntax": "postcss-styled",
     "plugins": [
       "stylelint-csstree-validator",
       "stylelint-declaration-block-no-ignored-properties",
